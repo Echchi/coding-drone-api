4d3ff679050a172331ed0be9a9db1059d87e9c2a
Echchi <112493260+Echchi@users.noreply.github.com>
2024-10-15 14:20:55 +0900

    cli 설치 및 app.module.ts 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Instructor } from './instructor/instructor.entity';
+import { Lectures } from './lectures/lectures.entity';
+import { Students } from './students/students.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: '3052',
+      database: 'coding_drone',
+      entities: [Instructor, Lectures, Students],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
