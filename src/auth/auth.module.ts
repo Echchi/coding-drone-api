ee149238ab321381c04a3470b2cc5cc57c958d4c
Echchi <112493260+Echchi@users.noreply.github.com>
2024-10-19 21:18:29 +0900

    jwt 설정 중

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { InstructorModule } from '../instructor/instructor.module';
 
 @Module({
+  imports: [
+    InstructorModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService]
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { InstructorService } from '../instructor/instructor.service';
 import { JwtService } from '@nestjs/jwt';
-import { UnauthorizedException } from "@nestjs/common";
+import { UnauthorizedException } from '@nestjs/common';
 
 export class AuthService {
   constructor(
     private instructorService: InstructorService,
     private jwtService: JwtService,
   ) {}
-  async signIn(username: string, password: string):Promise<{access_token : string}>{
+  async signIn(
+    username: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
     const instructor = await this.instructorService.findOne(username);
     if (
       !instructor ||
@@ -15,14 +18,16 @@ export class AuthService {
     ) {
       throw new UnauthorizedException('Invalid credentials');
     }
-  const payload = {username : instructor.username , sub: instructor.id};
+    const payload = { username: instructor.username, sub: instructor.id };
     return {
-    access_token : this.jwtService.sign(payload),
-  };
+      access_token: this.jwtService.sign(payload),
+    };
   }
 
-
-  private async validatePassword(inputPassword: string, savedPassword: any): Promise<boolean> {
+  private async validatePassword(
+    inputPassword: string,
+    savedPassword: any,
+  ): Promise<boolean> {
     return inputPassword === savedPassword;
   }
 }
