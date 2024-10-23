a9165462a86a7940de630917d37fd84194b0209c
Echchi <112493260+Echchi@users.noreply.github.com>
2024-10-23 14:40:04 +0900

    commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { InstructorModule } from '../instructor/instructor.module';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '30s' },
     }),
   ],
   providers: [AuthService],
